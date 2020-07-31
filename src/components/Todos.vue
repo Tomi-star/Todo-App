33a6affcb36e12a99cf114bfd1955bbfc8cb3ea6
Tomi-star <Tomisin.akinfemiwa@gmail.com>
2020-07-31 05:29:41 +0100

    Vue todo app initial commit

---
+++ src/components/Todos.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <div >
+
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"Todos",
+    props:["todos"]
+}
+</script>
+
+<style scoped>
+
+</style>
