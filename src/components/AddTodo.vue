e55acdf53ca02c0551d0d5811dd152cad2f8e540
Tomi-star <Tomisin.akinfemiwa@gmail.com>
2020-08-01 05:08:00 +0100

    Addtodo error fixed by adding v-model title to input area

--- src/components/AddTodo.vue
+++ src/components/AddTodo.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <form @submit="addTodo">
-            <input type="text" name="title" placeholder="Add Todo...">
+            <input type="text" v-model="title" name="title" placeholder="Add Todo...">
             <input type="submit" value="Submit" class="btn">
         </form>
     </div>
@@ -15,7 +15,7 @@ export default {
     name: "AddTodo",
     data() {
         return{
-            title: ''
+            title: ""
         }
     },
     methods: {
